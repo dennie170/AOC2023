3886a892695380b72f5c83e6e6af3af88ec59b3b
dennie170 <dennie170@gmail.com>
2023-12-06 17:54:54 +0100

    Solved Day 6 - part 2 by switching to Long

--- src/main/kotlin/com/dennie170/challenges/Day6.kt
+++ src/main/kotlin/com/dennie170/challenges/Day6.kt
@@ -2,16 +2,16 @@ package com.dennie170.challenges
 
 import com.dennie170.Day
 
-class Day6 : Day<Int>(6) {
+class Day6 : Day<Long>(6) {
     private val input = """
-Time:      7  15   30
-Distance:  9  40  200
+Time:        40     70     98     79
+Distance:   215   1051   2147   1005
     """.trimIndent()
 
 
-    override fun part1(): Int {
+    override fun part1(): Long {
         val lines = input.lines().map { it.substringAfter(':').trim() }
-            .map { it.split(' ').map { number -> number.trim() }.filter { number -> number.isNotEmpty() }.map { number -> number.toInt() } }
+            .map { it.split(' ').map { number -> number.trim() }.filter { number -> number.isNotEmpty() }.map { number -> number.toLong() } }
 
         val races = lines[0].zip(lines[1]).map {
             Race(it.first, it.second)
@@ -23,8 +23,8 @@ Distance:  9  40  200
             }
     }
 
-    private fun getPossibilities(race: Race): Int {
-        var possibilities = 0
+    private fun getPossibilities(race: Race): Long {
+        var possibilities = 0L
 
         for(chargeTime in 0 .. race.time) {
             val distance = calculateDistance(chargeTime, race.time - chargeTime)
@@ -37,13 +37,13 @@ Distance:  9  40  200
         return possibilities
     }
 
-    private fun calculateDistance(chargeTime: Int, remainingTime: Int): Int {
+    private fun calculateDistance(chargeTime: Long, remainingTime: Long): Long {
         return chargeTime * remainingTime
     }
 
-    override fun part2(): Int {
+    override fun part2(): Long {
         val race = input.lines().map { it.substringAfter(':').trim() }
-            .map { it.replace(" ", "").trim().toInt() }
+            .map { it.replace(" ", "").trim().toLong() }
             .zipWithNext().map {
                 Race(it.first, it.second)
             }.first()
@@ -51,5 +51,5 @@ Distance:  9  40  200
         return getPossibilities(race)
     }
 
-    data class Race(val time: Int, val record: Int)
+    data class Race(val time: Long, val record: Long)
 }
