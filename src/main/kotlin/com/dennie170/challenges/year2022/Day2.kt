c9bbe5078389d4ddc2c7fe2b3660a21628ec04bf
dennie170 <dennie170@gmail.com>
2023-12-08 22:38:08 +0100

    Refactored 2022 day 2

--- src/main/kotlin/com/dennie170/challenges/year2022/Day2.kt
+++ src/main/kotlin/com/dennie170/challenges/year2022/Day2.kt
@@ -12,59 +12,89 @@ class Day2 : Day<Int>(2022, 2) {
     }
 
     override fun part1(): Int {
-        return input.map { it.split(' ').map { c -> c.single().code }.toIntArray() }
-            .map {
-                when (23 + (it[0] - it[1])) {
-                    0 -> 3 + (it[1] - 87) // Draw
-                    -1, 2 -> 6 + (it[1] - 87) // Won
-                    -2, 1 -> 0 + (it[1] - 87) // Lost
+        return input.map {
+            val arr = it.split(' ').map { c -> c.single().code }.toIntArray()
 
-                    else -> throw IllegalStateException("${Char(it[0])} - ${Char(it[1])}")
+            val opp = when (arr[0]) {
+                65 -> Hand.ROCK
+                66 -> Hand.PAPER
+                67 -> Hand.SCISSORS
+                else -> throw RuntimeException()
+            }
+
+            val me = when (arr[1]) {
+                88 -> Hand.ROCK
+                89 -> Hand.PAPER
+                90 -> Hand.SCISSORS
+                else -> throw RuntimeException()
+            }
+
+            OneVersusOneMatch(opp, me)
+        }.map {
+            when (it.opp) {
+                Hand.ROCK -> when (it.me) {
+                    Hand.PAPER -> Result.WIN
+                    Hand.ROCK -> Result.DRAW
+                    Hand.SCISSORS -> Result.LOSE
                 }
-            }.reduce { acc, i -> acc + i }
+
+                Hand.PAPER -> when (it.me) {
+                    Hand.PAPER -> Result.DRAW
+                    Hand.ROCK -> Result.LOSE
+                    Hand.SCISSORS -> Result.WIN
+                }
+
+                Hand.SCISSORS -> when (it.me) {
+                    Hand.PAPER -> Result.LOSE
+                    Hand.ROCK -> Result.WIN
+                    Hand.SCISSORS -> Result.DRAW
+                }
+            }.score + it.me.value
+        }.reduce { acc, i -> acc + i }
     }
 
     override fun part2(): Int {
         return input.map {
-            val arr= it.split(' ').map { char ->
+            val arr = it.split(' ').map { char ->
                 char.single().code
             }.toIntArray()
 
-            Match(arr[0], Result.fromInt(arr[1]))
-        }.map { match  ->
+            MatchWithDesiredResult(arr[0], Result.fromInt(arr[1]))
+        }.map { match ->
             val outcomeScore = match.desiredResult.score
 
             val playedCard = getPlayedCardScore(match.opp, match.desiredResult)
 
             playedCard + outcomeScore
-        }.reduce { acc, i ->  acc + i}
+        }.reduce { acc, i -> acc + i }
     }
 
     private fun getPlayedCardScore(opp: Int, desiredResult: Result): Int {
-        return when(opp) {
-            65 -> when(desiredResult) { // ROCK
+        return when (opp) {
+            65 -> when (desiredResult) { // ROCK
                 Result.LOSE -> Hand.SCISSORS // Scissors
                 Result.DRAW -> Hand.ROCK // Rock
                 Result.WIN -> Hand.PAPER // Paper
             }
 
-            66  ->  when(desiredResult) { // PAPER
+            66 -> when (desiredResult) { // PAPER
                 Result.LOSE -> Hand.ROCK // Rock
                 Result.DRAW -> Hand.PAPER // Paper
                 Result.WIN -> Hand.SCISSORS // Scissors
             }
 
-            67 ->  when(desiredResult) { // SCISSORS
+            67 -> when (desiredResult) { // SCISSORS
                 Result.LOSE -> Hand.PAPER // Paper
                 Result.DRAW -> Hand.SCISSORS // Scissors
                 Result.WIN -> Hand.ROCK // Rock
             }
 
-            else ->  throw RuntimeException()
+            else -> throw RuntimeException()
         }.value
     }
 
-    data class Match(val opp: Int, val desiredResult: Result)
+    data class MatchWithDesiredResult(val opp: Int, val desiredResult: Result)
+    data class OneVersusOneMatch(val opp: Hand, val me: Hand)
 
     enum class Hand(val value: Int) {
         ROCK(1),
@@ -79,7 +109,7 @@ class Day2 : Day<Int>(2022, 2) {
 
         companion object {
             fun fromInt(input: Int): Result {
-                return when(input) {
+                return when (input) {
                     90 -> WIN // Z
                     89 -> DRAW // Y
                     88 -> LOSE // X
