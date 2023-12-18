23f0014e704ca04c292b6571843cccc450be8435
dennie170 <dennie170@gmail.com>
2023-12-18 21:52:01 +0100

    Solved Day 18 - part 2

--- src/main/kotlin/com/dennie170/challenges/year2023/Day18.kt
+++ src/main/kotlin/com/dennie170/challenges/year2023/Day18.kt
@@ -1,119 +1,71 @@
 package com.dennie170.challenges.year2023
 
 import com.dennie170.Day
-import java.awt.Polygon
+import kotlin.math.abs
 
 
-class Day18 : Day<Int>(2023, 18) {
+class Day18 : Day<Long>(2023, 18) {
 
-    private lateinit var input: Sequence<String>
+    private lateinit var input: List<String>
 
     override fun setUp() {
-        input = super.readInput().lineSequence()
+        input = super.readInput().lines()
     }
 
-    override fun part1(): Int {
-        val polygon = Polygon()
+    private var totalLength = 1L
 
-        var y = 0
-        var x = 0
+    private fun solve(instructions: List<Instruction>): Long {
+        val list = mutableListOf<Coordinate>()
 
-        for (instruction in input.map(Instruction::parse)) {
+        var y = 0L
+        var x = 0L
+
+        for (instruction in instructions) {
+            totalLength += instruction.steps
+
+            list.add(Coordinate(x, y))
+
             when (instruction.direction) {
                 Direction.UP -> {
-                    polygon.addPoint(y, x)
                     y += instruction.steps
                 }
 
                 Direction.RIGHT -> {
-                    polygon.addPoint(y, x)
                     x += instruction.steps
                 }
 
                 Direction.DOWN -> {
-                    polygon.addPoint(y, x)
                     y -= instruction.steps
                 }
 
                 Direction.LEFT -> {
-                    polygon.addPoint(y, x)
                     x -= instruction.steps
                 }
             }
         }
 
+        list.add(Coordinate(0, 0))
 
-        return getPolygonSurface(polygon)
-    }
-
-    private fun getPolygonSurface(polygon: Polygon): Int {
-        var total = 0
-
-        val x_min = polygon.xpoints.min() - 1
-        val x_max = polygon.xpoints.max() + 1
-
-        val y_min = polygon.ypoints.min() - 1
-        val y_max = polygon.ypoints.max() + 1
-
-        val zipped = polygon.xpoints.zip(polygon.ypoints).toSet()
-
-        val isWall: (x: Int, y: Int) -> Boolean = { x, y ->
-            zipped.windowed(2).any {
-                val xrange = if (it[0].first < it[1].first) it[0].first..it[1].first else it[0].first downTo it[1].first
-                val yrange = if (it[0].second < it[1].second) it[0].second..it[1].second else it[0].second downTo it[1].second
-
-                xrange.contains(x) && yrange.contains(y)
-            }
-        }
-
-        for (x in x_min..x_max) {
-            for (y in y_min..y_max) {
-                if (polygon.contains(x, y)) {
-                    total++
-                } else {
-                    if (isWall(x, y)) {
-                        total++
-                    }
-                }
-            }
-        }
-
-        return total
+        return getPolygonSurface(list)
     }
 
 
-    override fun part2(): Int {
-        return -1
-        val polygon = Polygon()
+    private fun getPolygonSurface(polygon: List<Coordinate>): Long {
+        val area = abs(polygon.windowed(2).sumOf {
+            (it[0].x * it[1].y) - (it[0].y * it[1].x) // Magic 😎
+        }) / 2
 
-        var y = 0
-        var x = 0
+        return area + (totalLength / 2) + 1
+    }
 
-        for (instruction in input.map(Instruction::parsePart2)) {
-            when (instruction.direction) {
-                Direction.UP -> {
-                    polygon.addPoint(y, x)
-                    y += instruction.steps
-                }
+    override fun part1(): Long {
+        totalLength = 1L
+        return solve(input.map(Instruction::parse))
+    }
 
-                Direction.RIGHT -> {
-                    polygon.addPoint(y, x)
-                    x += instruction.steps
-                }
-
-                Direction.DOWN -> {
-                    polygon.addPoint(y, x)
-                    y -= instruction.steps
-                }
-
-                Direction.LEFT -> {
-                    polygon.addPoint(y, x)
-                    x -= instruction.steps
-                }
-            }
-        }
-
-        return getPolygonSurface(polygon)
+    override fun part2(): Long {
+        totalLength = 1L
+        return solve(input.map(Instruction::parsePart2))
     }
 
 
@@ -161,4 +113,6 @@ class Day18 : Day<Int>(2023, 18) {
 
     @JvmInline
     private value class Colour(val colour: String)
+
+    private data class Coordinate(val x: Long, val y: Long)
 }
