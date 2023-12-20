1c12de95ee586e6f0675504e0b1271ac93c871c3
dennie170 <dennie170@gmail.com>
2023-12-20 09:04:17 +0100

    Started on Day 19 - part 2

--- src/main/kotlin/com/dennie170/challenges/year2023/Day19.kt
+++ src/main/kotlin/com/dennie170/challenges/year2023/Day19.kt
@@ -77,7 +77,62 @@ class Day19 : Day<Long>(2023, 19) {
     }
 
     override fun part2(): Long {
-        return -1
+        val (w, r) = input.split("\n\n")
+        workflows = w.lines().map(::mapWorkflow).associateBy { it.name }
+
+        val rating = mutableMapOf(
+            'x' to 1..4000,
+            'm' to 1..4000,
+            'a' to 1..4000,
+            's' to 1..4000,
+        )
+
+        return runRangeInto(rating.toMutableMap(), "in")
+    }
+
+    private fun runRangeInto(rating: MutableMap<Char, IntRange>, into: String): Long {
+        if (into == "R") return 0
+        if (into == "A") return getPossibilities(rating.toMutableMap())
+
+        val workflow = workflows[into]!!
+
+        var possibilities = 0L
+
+
+        for (rule in workflow.rules) {
+            val range = rating[rule.property]!!
+
+            if (rule.operator == '>') {
+                if (range.first > rule.value) return possibilities + runRangeInto(rating.toMutableMap(), rule.destination)
+
+                if (range.last > rule.value) {
+                    val newRating = rating.toMutableMap()
+                    newRating[rule.property] = rule.value + 1..range.last
+                    possibilities += runRangeInto(newRating.toMutableMap(), rule.destination)
+
+                    rating[rule.property] = range.first..rule.value
+                }
+            } else {
+                if (range.last < rule.value) return possibilities + runRangeInto(rating.toMutableMap(), rule.destination)
+
+                if (range.first < rule.value) {
+                    val newRating = rating.toMutableMap()
+                    newRating[rule.property] = range.first..<rule.value
+                    possibilities += runRangeInto(newRating.toMutableMap(), rule.destination)
+
+                    rating[rule.property] = range.first..rule.value
+                }
+            }
+
+        }
+
+        return possibilities
+    }
+
+    private fun getPossibilities(rating: Map<Char, IntRange>): Long {
+        return rating.values.fold(1L) { acc, range ->
+            acc * (range.last - range.first + 1)
+        }
     }
 
     private data class Workflow(val name: String, val rules: List<Rule>, val fallback: String)
