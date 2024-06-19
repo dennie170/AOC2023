658c8d899a92a55e8bb152364904dc5ea419d632
dennie170 <dennie170@gmail.com>
2024-06-19 19:54:24 +0200

    Optimized day 6 part 1 3-fold

--- src/main/kotlin/com/dennie170/challenges/year2022/Day6.kt
+++ src/main/kotlin/com/dennie170/challenges/year2022/Day6.kt
@@ -11,10 +11,13 @@ class Day6 : Day<Int>(2022, 6) {
         input = super.readInput().toCharArray().toList()
     }
 
+    // 1235
     override fun part1(): Int {
 
-        for((counter, window) in input.windowed(4).withIndex()) {
-            if(window.groupingBy { char -> char }.eachCount().size == 4) return counter + 4
+        for(index in 0..input.size) {
+            val set = setOf(input[index], input[index + 1], input[index + 2], input[index + 3])
+
+            if(set.size == 4) return index + 4
         }
 
        return -1
