f78f39ad03740e1cc5b0ecdcb94c2cf459cf0098
dennie170 <dennie170@gmail.com>
2024-12-02 16:58:25 +0100

    Solved 2024 day 2 part 2

--- src/main/kotlin/com/dennie170/challenges/year2024/Day2.kt
+++ src/main/kotlin/com/dennie170/challenges/year2024/Day2.kt
@@ -10,21 +10,53 @@ class Day2 : Day<Int>(2024, 2) {
 
     val lines: List<String> = readInput().lines()
 
-    override fun part1(): Int {
-        val reports = lines.map { it.split(' ') }.map {
-            it.map { r -> r.toInt() }
-        }
 
-       return reports.filter {
-           (it.areAllIncreasing() || it.areAllDecreasing())
-                   && !it.containsDuplicates()
-                   && it.maxDifferenceBetweenItems() <= 3
-
-       }.size
+    private fun parseReports() = lines.map { it.split(' ') }.map {
+        it.map { r -> r.toInt() }
     }
 
+    override fun part1(): Int {
+        val reports = parseReports()
+
+        return reports.filter {
+            (it.areAllIncreasing() || it.areAllDecreasing())
+                    && !it.containsDuplicates()
+                    && it.maxDifferenceBetweenItems() <= 3
+
+        }.size
+    }
 
     override fun part2(): Int {
-        return -1
+        val reports = parseReports()
+
+        return reports.filter {
+            bruteForceSafety(it, null)
+        }.size
     }
+
+    private fun bruteForceSafety(report: List<Int>, dropIndex: Int? = null): Boolean {
+        val r = if (dropIndex == null) report else report.toMutableList().apply {
+            if (dropIndex < size) removeAt(dropIndex)
+        }.toList()
+
+        // Regular safety
+        if ((r.areAllIncreasing() || r.areAllDecreasing())
+            && !r.containsDuplicates()
+            && r.maxDifferenceBetweenItems() <= 3
+        ) {
+
+            return true
+        }
+
+        if (dropIndex == null) {
+            for (i in 0..r.size) {
+                if (bruteForceSafety(r, i)) {
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
 }
