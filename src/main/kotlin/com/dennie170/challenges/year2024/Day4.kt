33c47a1f0a0ab6aecfae55db4064aa8b57feb81b
dennie170 <dennie170@gmail.com>
2024-12-05 21:22:49 +0100

    Made day 4 more efficient

--- src/main/kotlin/com/dennie170/challenges/year2024/Day4.kt
+++ src/main/kotlin/com/dennie170/challenges/year2024/Day4.kt
@@ -28,65 +28,72 @@ class Day4 : Day<Int>(2024, 4) {
     }
 
     private fun spellsXmas(startCoordinates: Coordinates, direction: Direction): Boolean {
-        return when {
-            direction == Direction.LEFT && startCoordinates.col - 3 >= 0 -> spellsXmasLeft(startCoordinates)
-            direction == Direction.RIGHT && startCoordinates.col + 3 < matrix.size -> spellsXmasRight(startCoordinates)
-            direction == Direction.UP && startCoordinates.row - 3 >= 0 -> spellsXmasUp(startCoordinates)
-            direction == Direction.DOWN && startCoordinates.row + 3 < matrix.size -> spellsXmasDown(startCoordinates)
-            direction == Direction.DIAGONAL_LEFT_UP && startCoordinates.row - 3 >= 0 && startCoordinates.col - 3 >= 0 -> spellsXmasDiagonalLeftUp(startCoordinates)
-            direction == Direction.DIAGONAL_RIGHT_UP && startCoordinates.row - 3 >= 0 && startCoordinates.col + 3 < matrix.size -> spellsXmasDiagonalRightUp(startCoordinates)
-            direction == Direction.DIAGONAL_LEFT_DOWN && startCoordinates.row + 3 < matrix.size && startCoordinates.col - 3 >= 0 -> spellsXmasDiagonalLeftDown(startCoordinates)
-            direction == Direction.DIAGONAL_RIGHT_DOWN && startCoordinates.row + 3 < matrix.size && startCoordinates.col + 3 < matrix.size -> spellsXmasDiagonalRightDown(startCoordinates)
-            else -> false
+        return when (direction) {
+            Direction.LEFT -> spellsXmasLeft(startCoordinates)
+            Direction.RIGHT -> spellsXmasRight(startCoordinates)
+            Direction.UP -> spellsXmasUp(startCoordinates)
+            Direction.DOWN -> spellsXmasDown(startCoordinates)
+            Direction.DIAGONAL_LEFT_UP -> spellsXmasDiagonalLeftUp(startCoordinates)
+            Direction.DIAGONAL_RIGHT_UP -> spellsXmasDiagonalRightUp(startCoordinates)
+            Direction.DIAGONAL_LEFT_DOWN -> spellsXmasDiagonalLeftDown(startCoordinates)
+            Direction.DIAGONAL_RIGHT_DOWN -> spellsXmasDiagonalRightDown(startCoordinates)
         }
     }
 
     private fun spellsXmasLeft(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row][startCoordinates.col - 1] == 'M'
+        return startCoordinates.col - 3 >= 0
+                && matrix[startCoordinates.row][startCoordinates.col - 1] == 'M'
                 && matrix[startCoordinates.row][startCoordinates.col - 2] == 'A'
                 && matrix[startCoordinates.row][startCoordinates.col - 3] == 'S'
     }
 
     private fun spellsXmasRight(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row][startCoordinates.col + 1] == 'M'
+        return startCoordinates.col + 3 < matrix.size
+                && matrix[startCoordinates.row][startCoordinates.col + 1] == 'M'
                 && matrix[startCoordinates.row][startCoordinates.col + 2] == 'A'
                 && matrix[startCoordinates.row][startCoordinates.col + 3] == 'S'
     }
 
     private fun spellsXmasUp(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row - 1][startCoordinates.col] == 'M'
+        return startCoordinates.row - 3 >= 0
+                && matrix[startCoordinates.row - 1][startCoordinates.col] == 'M'
                 && matrix[startCoordinates.row - 2][startCoordinates.col] == 'A'
                 && matrix[startCoordinates.row - 3][startCoordinates.col] == 'S'
     }
 
 
     private fun spellsXmasDown(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row + 1][startCoordinates.col] == 'M'
+        return startCoordinates.row + 3 < matrix.size
+                && matrix[startCoordinates.row + 1][startCoordinates.col] == 'M'
                 && matrix[startCoordinates.row + 2][startCoordinates.col] == 'A'
                 && matrix[startCoordinates.row + 3][startCoordinates.col] == 'S'
     }
 
     private fun spellsXmasDiagonalLeftUp(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row - 1][startCoordinates.col - 1] == 'M'
+        return startCoordinates.row - 3 >= 0 && startCoordinates.col - 3 >= 0
+                && matrix[startCoordinates.row - 1][startCoordinates.col - 1] == 'M'
                 && matrix[startCoordinates.row - 2][startCoordinates.col - 2] == 'A'
                 && matrix[startCoordinates.row - 3][startCoordinates.col - 3] == 'S'
     }
 
     private fun spellsXmasDiagonalRightUp(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row - 1][startCoordinates.col + 1] == 'M'
+        return startCoordinates.row - 3 >= 0 && startCoordinates.col + 3 < matrix.size
+                && matrix[startCoordinates.row - 1][startCoordinates.col + 1] == 'M'
                 && matrix[startCoordinates.row - 2][startCoordinates.col + 2] == 'A'
                 && matrix[startCoordinates.row - 3][startCoordinates.col + 3] == 'S'
     }
 
 
     private fun spellsXmasDiagonalLeftDown(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row + 1][startCoordinates.col - 1] == 'M'
+        return startCoordinates.row + 3 < matrix.size && startCoordinates.col - 3 >= 0
+                && matrix[startCoordinates.row + 1][startCoordinates.col - 1] == 'M'
                 && matrix[startCoordinates.row + 2][startCoordinates.col - 2] == 'A'
                 && matrix[startCoordinates.row + 3][startCoordinates.col - 3] == 'S'
     }
 
     private fun spellsXmasDiagonalRightDown(startCoordinates: Coordinates): Boolean {
-        return matrix[startCoordinates.row + 1][startCoordinates.col + 1] == 'M'
+        return startCoordinates.row + 3 < matrix.size && startCoordinates.col + 3 < matrix.size
+                && matrix[startCoordinates.row + 1][startCoordinates.col + 1] == 'M'
                 && matrix[startCoordinates.row + 2][startCoordinates.col + 2] == 'A'
                 && matrix[startCoordinates.row + 3][startCoordinates.col + 3] == 'S'
     }
@@ -94,9 +101,9 @@ class Day4 : Day<Int>(2024, 4) {
     override fun part1(): Int {
         val xCoordinates = mutableSetOf<Coordinates>()
 
-        for ((rowIndex, row) in matrix.withIndex()) {
-            for ((colIndex, col) in row.withIndex()) {
-                if (col == 'X') xCoordinates.add(Coordinates(rowIndex, colIndex))
+        for (rowIndex in matrix.indices) {
+            for (colIndex in matrix[rowIndex].indices) {
+                if (matrix[rowIndex][colIndex] == 'X') xCoordinates.add(Coordinates(rowIndex, colIndex))
             }
         }
 
@@ -112,13 +119,12 @@ class Day4 : Day<Int>(2024, 4) {
         return matches
     }
 
-    // 2859 -> too high
     override fun part2(): Int {
         val aCoordinates = mutableSetOf<Coordinates>()
 
-        for ((rowIndex, row) in matrix.withIndex()) {
-            for ((colIndex, col) in row.withIndex()) {
-                if (col == 'A') aCoordinates.add(Coordinates(rowIndex, colIndex))
+        for (rowIndex in matrix.indices) {
+            for (colIndex in matrix[rowIndex].indices) {
+                if (matrix[rowIndex][colIndex] == 'A') aCoordinates.add(Coordinates(rowIndex, colIndex))
             }
         }
 
@@ -129,9 +135,8 @@ class Day4 : Day<Int>(2024, 4) {
                         && (matrix[coordinates.row + 1][coordinates.col - 1].isEither('M', 'S'))
                         && (matrix[coordinates.row + 1][coordinates.col + 1].isEither('M', 'S'))
                         && (matrix[coordinates.row + 1][coordinates.col - 1] != matrix[coordinates.row - 1][coordinates.col + 1]) // left bottom may not match right top
-                        && (matrix[coordinates.row - 1][coordinates.col - 1] != matrix[coordinates.row + 1][coordinates.col + 1])) // left bottom may not match right top
-                // left top may not match right bottom
-            } catch(_: ArrayIndexOutOfBoundsException) {
+                        && (matrix[coordinates.row - 1][coordinates.col - 1] != matrix[coordinates.row + 1][coordinates.col + 1])) // left top may not match right bottom
+            } catch (_: ArrayIndexOutOfBoundsException) {
                 false
             }
         }
