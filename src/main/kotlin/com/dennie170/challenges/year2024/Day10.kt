0fb7577d032dbffedcafce1ffa88b53fd13158ee
dennie170 <dennie170@gmail.com>
2024-12-10 21:46:22 +0100

    Huge improvement for day 10

--- src/main/kotlin/com/dennie170/challenges/year2024/Day10.kt
+++ src/main/kotlin/com/dennie170/challenges/year2024/Day10.kt
@@ -11,7 +11,7 @@ class Day10 : Day<Int>(2024, 10) {
 
     override fun setUp() {
         input = super.readInput().replace("\n", "")
-        matrix = getMatrix(input.toCharArray().map { if(it == '.') -1 else it.digitToInt() })
+        matrix = getMatrix(input.toCharArray().map { if (it == '.') -1 else it.digitToInt() })
     }
 
     private val reachedPeaks = mutableSetOf<Pair<Coordinates, Coordinates>>()
@@ -20,86 +20,38 @@ class Day10 : Day<Int>(2024, 10) {
 
     private val trialHeadScores = mutableMapOf<Coordinates, Int>()
 
-
     // Recursive function to walk the trial
     // Coordinates do not need to be a 0. Can also pick up a new trail
     private fun walkTrial(trialHead: Coordinates, coordinates: Coordinates = trialHead) {
         val currentValue = matrix[coordinates.row][coordinates.col]
 
-        if (coordinates.row + 1 < matrix.size && matrix[coordinates.row + 1][coordinates.col] == currentValue + 1) {
-            if (matrix[coordinates.row + 1][coordinates.col] == 9) {
+        for (rowDiff in -1..1) {
+            for (colDiff in -1..1) {
+                if ((rowDiff == 0 && colDiff == 0) || (rowDiff != 0 && colDiff != 0)) continue
 
+                if (coordinates.row + rowDiff < 0 || coordinates.row + rowDiff >= matrix.size) continue
+                if (coordinates.col + colDiff < 0 || coordinates.col + colDiff >= matrix.size) continue
 
-                if (!reachedPeaks.contains(trialHead to coordinates.copy(row = coordinates.row + 1))) {
-                    reachedPeaks.add(trialHead to coordinates.copy(row = coordinates.row + 1))
+                if (matrix[coordinates.row + rowDiff][coordinates.col + colDiff] != currentValue + 1) continue
 
-                    val trialHeadIndex = trialHeads.indexOfFirst { it.first == trialHead }
-                    val head = trialHeads[trialHeadIndex]
-                    trialHeads[trialHeadIndex] = head.copy(second = head.second + 1)
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                } else {
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                }
+                if (matrix[coordinates.row + rowDiff][coordinates.col + colDiff] == 9) {
+                    if (!reachedPeaks.contains(trialHead to coordinates.copy(row = coordinates.row + rowDiff, col = coordinates.col + colDiff))) {
+                        reachedPeaks.add(trialHead to coordinates.copy(row = coordinates.row + rowDiff, col = coordinates.col + colDiff))
 
-            } else walkTrial(trialHead, Coordinates(coordinates.row + 1, coordinates.col))
+                        val trialHeadIndex = trialHeads.indexOfFirst { it.first == trialHead }
+                        val head = trialHeads[trialHeadIndex]
+                        trialHeads[trialHeadIndex] = head.copy(second = head.second + 1)
+                    }
+                    if (part == 2) {
+                        trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
+                    }
+
+                } else walkTrial(trialHead, Coordinates(coordinates.row + rowDiff, coordinates.col + colDiff))
+            }
         }
-
-
-        if (coordinates.row - 1 >= 0 && matrix[coordinates.row - 1][coordinates.col] == currentValue + 1) {
-            if (matrix[coordinates.row - 1][coordinates.col] == 9) {
-
-                if (!reachedPeaks.contains(trialHead to coordinates.copy(row = coordinates.row - 1))) {
-                    reachedPeaks.add(trialHead to coordinates.copy(row = coordinates.row - 1))
-
-                    val trialHeadIndex = trialHeads.indexOfFirst { it.first == trialHead }
-                    val head = trialHeads[trialHeadIndex]
-                    trialHeads[trialHeadIndex] = head.copy(second = head.second + 1)
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                } else {
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                }
-
-            } else walkTrial(trialHead, Coordinates(coordinates.row - 1, coordinates.col))
-        }
-
-
-        if (coordinates.col + 1 < matrix.size && matrix[coordinates.row][coordinates.col + 1] == currentValue + 1) {
-            if (matrix[coordinates.row][coordinates.col + 1] == 9) {
-
-                if (!reachedPeaks.contains(trialHead to coordinates.copy(col = coordinates.col + 1))) {
-                    reachedPeaks.add(trialHead to coordinates.copy(col = coordinates.col + 1))
-
-                    val trialHeadIndex = trialHeads.indexOfFirst { it.first == trialHead }
-                    val head = trialHeads[trialHeadIndex]
-                    trialHeads[trialHeadIndex] = head.copy(second = head.second + 1)
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                } else {
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                }
-
-
-            } else walkTrial(trialHead, Coordinates(coordinates.row, coordinates.col + 1))
-        }
-
-        if (coordinates.col - 1 >= 0 && matrix[coordinates.row][coordinates.col - 1] == currentValue + 1) {
-            if (matrix[coordinates.row][coordinates.col - 1] == 9) {
-
-                if (!reachedPeaks.contains(trialHead to coordinates.copy(col = coordinates.col - 1))) {
-                    reachedPeaks.add(trialHead to coordinates.copy(col = coordinates.col - 1))
-
-                    val trialHeadIndex = trialHeads.indexOfFirst { it.first == trialHead }
-                    val head = trialHeads[trialHeadIndex]
-                    trialHeads[trialHeadIndex] = head.copy(second = head.second + 1)
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                } else {
-                    trialHeadScores[trialHead] = trialHeadScores[trialHead]!! + 1
-                }
-
-            } else walkTrial(trialHead, Coordinates(coordinates.row, coordinates.col - 1))
-        }
-
     }
 
+    private var part = 1
     override fun part1(): Int {
 
         for (row in matrix.indices) {
@@ -107,8 +59,6 @@ class Day10 : Day<Int>(2024, 10) {
                 if (matrix[row][col] != 0) continue // Only filter on starts
 
                 trialHeads.add(Pair(Coordinates(row, col), 0))
-                trialHeadScores[Coordinates(row, col)] = 0
-
                 walkTrial(Coordinates(row, col))
             }
         }
@@ -117,6 +67,7 @@ class Day10 : Day<Int>(2024, 10) {
     }
 
     override fun part2(): Int {
+        part = 2
 
         trialHeads.clear()
         trialHeadScores.clear()
