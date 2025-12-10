fb3bc087a7204a8dc5aeee6caa5925c27c9effb2
dennie170 <dennie170@gmail.com>
2025-12-10 09:36:56 +0100

    Made 2025 Day 9 part 1 a bit nicer

--- src/main/kotlin/com/dennie170/challenges/year2025/Day9.kt
+++ src/main/kotlin/com/dennie170/challenges/year2025/Day9.kt
@@ -14,19 +14,11 @@ class Day9 : Day<Long>(2025, 9) {
             it.split(',').let { (col, row) -> Tile(row.toInt(), col.toInt()) }
         }
 
-        var maxSurface = 0L
-
-        for (tile in tiles) {
-            for (b in tiles) {
-                val surface = (abs(b.row - tile.row) + 1).toLong() * (abs(tile.col - b.col) + 1).toLong()
-
-                if (surface > maxSurface) {
-                    maxSurface = surface
-                }
+        return tiles.maxOf { tile ->
+            tiles.maxOf { b ->
+                (abs(b.row - tile.row) + 1).toLong() * (abs(tile.col - b.col) + 1).toLong()
             }
         }
-
-        return maxSurface
     }
 
     override fun part2(): Long {
